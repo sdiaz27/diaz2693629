ebf04486c3b7609e59e91f90124afec18c37c8a3
sdiaz <sebastiandiazlache@gmail.com>
2023-05-03 11:42:58 -0500

    list + epomedio mayr q menor q mediana

--- listas/list2.py
+++ listas/list2.py
@@ -5,8 +5,10 @@ sum=0
 pro=0
 may=0
 men=999
+mida=0
 for i in range(tem):
-    num=int(random.randrange(100))
+    #num=int(random.randrange(100))
+    num=(random.random()*100)
     lista.append(num)
     sum+=num
     pro=sum/tem
@@ -14,10 +16,15 @@ for i in range(tem):
         may=num
     if num < men:
         men=num
+    if num >0.0 :
+        mida=(may + men)/2
+    
+        
 print(lista)
 print (f'lasumadelosnumeros es : {sum} ' )
 print (f'le promedio de los  mumero es :  {pro}')
 print (f'el numero mayor es :  {may}')
 print (f'el numero menor es :  {men}')
+print (f'la mediana es  :  {mida}')
 
 
