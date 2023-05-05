9aa6fb53242df3bd8419076f8ec6f89ab2a0d5ad
sdiaz27 <sebastiandiazlache@gmail.com>
2023-05-05 07:30:19 -0500

    se sube lista 2

--- listas/list2.py
+++ listas/list2.py
@@ -1,30 +1,49 @@
 import random
-lista=[]
-tem=int(random.randint(10,20))
-sum=0
-pro=0
-may=0
-men=999
-mida=0
+
+lista = []
+tem = int(random.randint(10, 20))
+sum = 0
+pro = 0
+may = 0
+men = 999
+mida = 0
+
+# Calcula la lista de números y realiza otros cálculos
 for i in range(tem):
-    #num=int(random.randrange(100))
-    num=(random.random()*100)
+    num =(random.random(100))
     lista.append(num)
-    sum+=num
-    pro=sum/tem
+    sum += num
+    pro = sum / tem
     if num > may:
-        may=num
+        may = num
     if num < men:
-        men=num
-    if num >0.0 :
-        mida=(may + men)/2
-    
-        
+        men = num
+
+# Calcula la moda
+frecuencias = {}
+for num in lista:
+    if num in frecuencias:
+        frecuencias[num] += 1
+    else:
+        frecuencias[num] = 1
+
+max_frecuencia = max(frecuencias.values())
+moda = min([k for k, v in frecuencias.items() if v == max_frecuencia])
+
+# Calcula la mediana
+lista.sort()
+n = len(lista)
+if n % 2 == 0:
+    mediana = (lista[n // 2 - 1] + lista[n // 2]) / 2
+else:
+    mediana = lista[n // 2]
+
 print(lista)
-print (f'lasumadelosnumeros es : {sum} ' )
-print (f'le promedio de los  mumero es :  {pro}')
-print (f'el numero mayor es :  {may}')
-print (f'el numero menor es :  {men}')
-print (f'la mediana es  :  {mida}')
+print(f'La suma de los números es: {sum}')
+print(f'El promedio de los números es: {pro}')
+print(f'El número mayor es: {may}')
+print(f'El número menor es: {men}')
+print(f'La mediana es: {mediana}')
+print(f'La moda es: {moda}')
 
 
